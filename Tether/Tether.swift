07d5901452eb58488803eee9aa23fec3acf685e9
Dmytro Dovgoshliubnyi <dmytro@dovgo.sh>
2026-03-16 14:43:26 +0000

    Initial Commit

---
+++ Tether/Tether.swift
@@ -0,0 +1,9 @@
+//
+//  Tether.swift
+//  Tether
+//
+//  Created by Dmytro Dovgoshliubnyi on 2026-03-16.
+//
+
+import Foundation
+
---
+++ TetherTests/TetherTests.swift
@@ -0,0 +1,17 @@
+//
+//  TetherTests.swift
+//  TetherTests
+//
+//  Created by Dmytro Dovgoshliubnyi on 2026-03-16.
+//
+
+import Testing
+@testable import Tether
+
+struct TetherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
